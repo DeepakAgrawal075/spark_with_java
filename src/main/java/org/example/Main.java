3f19c73e6de3e65a2f9ad09e1d12f3d0a8fafc4d
Deepak Agrawal <bits.deepak075@gmail.com>
2024-05-04 13:59:46 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession
+                .builder()
+                .master("local[*]")
+                .getOrCreate();
+        Dataset<Row> datasetOfRowObjects = spark
+                .read()
+                .format("json")
+                .load("/Applications/MAMP/htdocs/Spark-The-Definitive-Guide/data/flight-data/json/2015-summary.json");
+        List<Row> rows = datasetOfRowObjects.collectAsList();
+        System.out.println(rows.size());
+    }
+}
