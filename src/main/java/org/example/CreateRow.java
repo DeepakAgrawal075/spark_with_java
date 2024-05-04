b2695e47b3d4a631bdd5063373978b4ec640e944
Deepak Agrawal <bits.deepak075@gmail.com>
2024-05-04 17:33:15 -0400

    CreateRow

--- src/main/java/org/example/CreateRow.java
+++ src/main/java/org/example/CreateRow.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import com.google.common.collect.ImmutableList;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
@@ -21,10 +23,12 @@ public class CreateRow {
                         DataTypes.createStructField("last_name", DataTypes.StringType, false)
                 }
         );
-        Row r1 = RowFactory.create("Deepak", "Agrawal");
-        Row r2 = RowFactory.create("Robin", "Sharma");
+        Row r1 = RowFactory.create("Alice", "Henderson");
+        Row r2 = RowFactory.create("Bob", "Sanders");
         ImmutableList<Row> data = ImmutableList.of(r1, r2);
         Dataset<Row> rowDataset = spark.createDataFrame(data, schema);
         rowDataset.show();
+        JavaRDD<Row> distributedData = new JavaSparkContext(spark.sparkContext()).parallelize(data);
+        System.out.println(distributedData.count());
     }
 }
